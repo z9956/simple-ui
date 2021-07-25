f15a44da7d9e638cb56765e07bb4f0a0bfa28e90
9956 <zhz995617@gmail.com>
2021-07-26 00:41:47 +0800

    chore: 异常抛出

--- scripts/build.js
+++ scripts/build.js
@@ -3,6 +3,15 @@ const webpack = require('webpack');
 const configFactory = require('../config/webpack.config');
 const config = configFactory('production');
 
-webpack(config).run((err) => {
-	console.log(err);
+console.log('Creating an optimized production build...');
+
+webpack(config).run((err, status) => {
+	let messages;
+
+	if (err) {
+		messages = err.message;
+	} else {
+		messages = status.toJson({ all: false, warnings: true, errors: true });
+	}
+	console.log(messages);
 });
