a4ba052f9df8c204ac4d5d7037a1f85878470776
9956 <zhz995617@gmail.com>
2021-08-31 00:24:55 +0800

    chore: storybook配置修改

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,53 @@
+const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin');
+
 module.exports = {
 	stories: [
-		'../stories/**/*.stories.mdx',
-		'../stories/**/*.stories.@(js|jsx|ts|tsx)',
+		'../components/**/*.stories.mdx',
+		'../components/**/*.stories.@(js|jsx|ts|tsx|mdx)',
 	],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-knobs/register',
+		{
+			name: '@storybook/addon-docs', // new addon for docs
+			options: {
+				configureJSX: true,
+				babelOptions: {},
+				sourceLoaderOptions: null,
+				transcludeMarkdown: true,
+			},
+		},
+	],
+	webpackFinal: async (config) => {
+		config.module.rules.push({
+			// 2a. Load `.stories.mdx` / `.story.mdx` files as CSF and generate
+			//     the docs page from the markdown
+			test: /\.(stories|story)\.mdx$/,
+			use: [
+				{
+					loader: 'babel-loader',
+					// may or may not need this line depending on your app's setup
+					options: {
+						plugins: ['@babel/plugin-transform-react-jsx'],
+					},
+				},
+				{
+					loader: '@mdx-js/loader',
+					options: {
+						compilers: [createCompiler({})],
+					},
+				},
+			],
+		});
+		config.module.rules.push({
+			test: /\.(stories|story)\.[tj]sx?$/,
+			loader: require.resolve('@storybook/source-loader'),
+			exclude: [/node_modules/],
+			enforce: 'pre',
+		});
+		return config;
+	},
 	core: {
 		builder: 'webpack5',
 	},
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,15 @@
+import React from 'react';
+
 export const parameters = {
-	actions: { argTypesRegex: '^on[A-Z].*' },
-	controls: {
-		matchers: {
-			color: /(background|color)$/i,
-			date: /Date$/,
-		},
+	// automatically create action args for all props that start with "on"
+	actions: { argTypesRegex: '^on.*' },
+	dependencies: {
+		// display only dependencies/dependents that have a story in storybook
+		// by default this is false
+		withStoriesOnly: true,
+
+		// completely hide a dependency/dependents block if it has no elements
+		// by default this is false
+		hideEmpty: true,
 	},
 };
