c6abfabd85ae4ddc764468de49978c073ddc072f
9956 <zhz995617@gmail.com>
2021-09-01 22:22:16 +0800

    feat: 新增Button示例

---
+++ components/button/Button.stories.tsx
@@ -0,0 +1,59 @@
+import React, { Fragment } from 'react';
+import { Meta, Story } from '@storybook/react';
+import { css } from '@emotion/css';
+
+import Button, { ButtonProps } from '../button';
+import { ButtonTypes } from '../button/button';
+import { SizeType } from '../config/size';
+
+const styles = {
+	group: css({
+		marginTop: 20,
+	}),
+	button: css`
+		margin-right: 10px;
+	`,
+};
+
+export default {
+	title: 'Example/Button',
+	component: Button,
+	argTypes: {
+		variant: {
+			Description: '',
+			Default: '',
+		},
+	},
+} as Meta;
+
+export const Basic: Story<ButtonProps> = () => {
+	const sizes: SizeType[] = ['sm', 'md', 'lg'];
+	const buttonTypes: ButtonTypes[] = ['secondary', 'primary', 'error'];
+
+	return (
+		<div>
+			<div>
+				{buttonTypes.map((type) => {
+					return (
+						<div className={styles.group}>
+							{sizes.map((size) => {
+								const text = `${type} ${size}`;
+								return (
+									<Fragment key={text}>
+										<Button
+											className={styles.button}
+											variant={type}
+											size={size}
+										>
+											{text}
+										</Button>
+									</Fragment>
+								);
+							})}
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
