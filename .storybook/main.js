285137be2ba93519455ffadc7cb826742d305ec8
z9956 <zhz995617@gmail.com>
2021-09-17 17:59:44 +0800

    chore: storybook配置修改

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,54 +1,51 @@
-const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin');
-
 module.exports = {
 	stories: [
-		'../components/**/*.stories.mdx',
-		'../components/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+		// '../components/**/*.stories.mdx',
+		'../components/**/*.stories.@(js|jsx|ts|tsx)',
 	],
-	addons: [
-		{
-			name: '@storybook/addon-docs', // new addon for docs
-			options: {
-				configureJSX: true,
-				babelOptions: {},
-				sourceLoaderOptions: null,
-				transcludeMarkdown: true,
-			},
-		},
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-knobs/register',
-	],
-	webpackFinal: async (config) => {
-		config.module.rules.push({
-			// 2a. Load `.stories.mdx` / `.story.mdx` files as CSF and generate
-			//     the docs page from the markdown
-			test: /\.(stories|story)\.mdx$/,
-			use: [
-				{
-					loader: 'babel-loader',
-					// may or may not need this line depending on your app's setup
-					options: {
-						plugins: ['@babel/plugin-transform-react-jsx'],
-					},
-				},
-				{
-					loader: '@mdx-js/loader',
-					options: {
-						compilers: [createCompiler({})],
-					},
-				},
-			],
-		});
-		config.module.rules.push({
-			test: /\.(stories|story)\.[tj]sx?$/,
-			loader: require.resolve('@storybook/source-loader'),
-			exclude: [/node_modules/],
-			enforce: 'pre',
-		});
-		return config;
-	},
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 	core: {
 		builder: 'webpack5',
+	},
+};
+
+module.exports = {
+	stories: ['../components/**/*.stories.@(mdx|js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-essentials',
+		'@storybook/addon-storysource',
+		'@storybook/addon-a11y',
+	],
+	skeletonWebpackConfig: {
+		module: {
+			rules: [
+				{
+					test: /\.m?[t|j]sx?$/,
+					resolve: {
+						fullySpecified: false,
+					},
+				},
+				// {
+				// 	test: /\.css$/,
+				// 	use: ['style-loader', 'css-loader'],
+				// },
+				{
+					test: /\.(png|jpe?g|gif|svg)$/i,
+					use: [
+						{
+							loader: 'file-loader',
+						},
+					],
+				},
+				{
+					test: /\.mdx/,
+					use: [
+						{
+							loader: 'null-loader',
+						},
+					],
+				},
+			],
+		},
 	},
 };
