4a2fec7f4d92494da83f00639e0d09f9b4edd9bc
z9956 <zhz995617@gmail.com>
2022-02-01 15:35:05 +0800

    chore: 依赖更新

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,12 @@ module.exports = {
 		'../components/**/*.stories.mdx',
 		'../components/**/*.stories.@(js|jsx|ts|tsx)',
 	],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+	addons: [
+		'@storybook/addon-docs',
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-controls',
+	],
 	core: {
 		builder: 'webpack5',
 	},
